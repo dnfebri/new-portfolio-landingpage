412fcccaf2186ae08f1c8573f622b41f86b04b35
dnfebri <dwinofebri@gmail.com>
2023-05-19 19:12:55 +0700

    feat: Project Landingpage

---
+++ src/constant/main.ts
@@ -0,0 +1,33 @@
+export const WORK_PROJECT = [
+  {
+    linkApp: "https://dnfebri-profile.vercel.app/",
+    image: "/images/dnfebri.png",
+    linkCode: "#",
+  },
+  {
+    linkApp: "https://web-fitnessworks.vercel.app/",
+    image: "/images/web-fw.png",
+    linkCode: "https://github.com/dnfebri/next-web-fw",
+  },
+  {
+    linkApp: "https://ikan.dnfebri.com/",
+    image: "/images/perikanan.png",
+    linkCode: "https://github.com/dnfebri/perikanan",
+  },
+  {
+    linkApp: "https://dnfebri-post.netlify.app/",
+    image: "/images/Simple-sosmed.png",
+    linkCode: "https://github.com/dnfebri/mv_frontend",
+  },
+  {
+    linkApp: "https://urbanathletes.co.id/",
+    image: "/images/urbanathletes.png",
+    linkCode: "https://github.com/dnfebri/kemitraan-urbanathletes",
+  },
+  {
+    linkApp:
+      "https://drive.google.com/file/d/1gZymXQQSAF0ltKw6zsnnkNFCwrBJxag-/view?usp=share_link",
+    image: "/images/firstMobileApp.png",
+    linkCode: "https://github.com/dnfebri/test-dss-react_native",
+  },
+];
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: "class",
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      maxWidth: {
+        "8xl": "1440px",
+      },
+      colors: {
+        AAprimary: "#0b192f",
+        AAsecondary: "#64ffda",
+        AAError: "#ff6489",
+        AAtertiary: "#112340",
+        ResumeButtonHover: "#153040",
+        MobileNavBarColor: "#112340",
+        StartupBackground: "#020c1b",
       },
     },
   },
   plugins: [],
-}
+};
